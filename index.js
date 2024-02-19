f7fc45ffd419480ab801c3164763b0259439e5b8
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-02-19 12:47:20 +0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const { product, users } = require("./dummy.json");
+
+app.get("/abc", (request, response) => {
+  response.type("application/json");
+  response.send("Hello");
+});
+
+app.get("/product", (request, response) => {
+  response.type("application/json");
+  response.send({ product, users });
+});
+
+app.get("/names", (request, response) => {
+  response.type("application/json");
+  response.send({ names });
+});
+
+app.listen("3001", () => {
+  console.log("Server is listening at port 3001");
+});
+
+let names = [];
+names = users.map((e) => {
+  console.log("e: ", e.name);
+  return e.name;
+});
+
+console.log("names: ", names);
